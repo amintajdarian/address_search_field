2d27f95d458ceac0fb8e9c444f041894966eef06
amintajdarian <amintajdarian@gmail.com>
2024-03-28 01:39:32 +0330

    update http ser

--- lib/src/utils/http_client.dart
+++ lib/src/utils/http_client.dart
@@ -2,8 +2,8 @@ part of 'package:address_search_field/src/services/geo_methods.dart';
 
 Future<Map<String, dynamic>> _httpClient(Uri uri) async {
   final baseOptions = BaseOptions(
-    connectTimeout: 5000,
-    receiveTimeout: 5000,
+    connectTimeout: Duration.secondsPerMinute,
+    receiveTimeout: Duration.secondsPerMinute,
     contentType: 'application/json',
     responseType: ResponseType.json,
   );
